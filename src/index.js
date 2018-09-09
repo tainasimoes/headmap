a5bb805e353a5e02946e2056e1854a3b4d57de72
Taina Simoes <taina.simoes.santos@gmail.com>
2018-09-09 18:00:04 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import anime from 'animejs'
+
+import './scss/style.scss';
+
+console.log(process.env.NODE_ENV);
+
+/*$('#header').addClass('ready');
+
+anime({
+  targets: '#header',
+  translateY: [
+    { value: 20, duration: 1000 },
+    { value: 0, duration: 1000 },
+  ],
+  duration: 2000,
+  loop: true
+});*/
+
